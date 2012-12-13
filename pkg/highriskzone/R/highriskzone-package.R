4c692a2d3b77dd72a16e5611c8b5de0cdcec1d2a
heidi <heidi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-13 13:31:03 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/highriskzone@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/highriskzone/R/highriskzone-package.R
@@ -0,0 +1,35 @@
+
+#' The highriskzone-package provides tools to determine and evaluate high-risk zones of 
+#' unobserved events using point process methodology. 
+#'
+#' \tabular{ll}{
+#' Package: \tab highriskzone\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0\cr
+#' Date: \tab 2012-07-11\cr
+#' License: \tab MIT\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' @author
+#' Heidi Seibold \email{Heidi.Seibold@@campus.lmu.de},
+#' Monia Mahling \email{monia.mahling@@stat.uni-muenchen.de}
+#' 
+#'
+#' Maintainer: Heidi Seibold \email{Heidi.Seibold@@campus.lmu.de}
+#' @name highriskzone-package
+#' @docType package
+#' @aliases highriskzone package-highriskzone
+#' @import spatstat ks
+#' @title Determining high risk zones using point process methodology
+#' @keywords package
+#' @references Monia Mahling, Michael \enc{Höhle}{Hoehle}& Helmut \enc{Küchenhoff}{Kuechenhoff} (expected 2013), 
+#' \emph{Determining high-risk zones for unexploded World War II bombs by using point process methodology.}
+#' expected in Journal of the Royal Statistical Society, Series C. \cr
+#' 
+#' Heidi Seibold (2012), \emph{Determining high risk zones using point process methodology 
+#' - Realization by building an R package.} Bachelor Thesis, Ludwig Maximilian University of Munich.
+#' @seealso  \code{\link[spatstat]{spatstat-package}}
+#' @encoding latin1
+#' 
+NULL
